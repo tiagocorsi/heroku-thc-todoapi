4d94d4359b91f04d27c61bed0d0eb56b786802c3
Tiago Henrique Corsi <tiagocorsi@gmail.com>
2022-02-08 10:14:11 -0300

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -30,7 +30,7 @@ namespace TodoApi.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
+                TemperatureC = rng.Next(-20, 50),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
